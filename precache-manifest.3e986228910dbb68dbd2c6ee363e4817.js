5a99ad9dccba22921fe847ce9732b921cdcd6d2a
Sovattha Sok <sok.sovattha@moviplus.ch>
2020-03-17 23:41:13 +0100

    Updates

---
+++ precache-manifest.3e986228910dbb68dbd2c6ee363e4817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf6f424721ee9ae5c45b04c1d66902e",
+    "url": "/tech-building/index.html"
+  },
+  {
+    "revision": "784378a911200fc2fb0c",
+    "url": "/tech-building/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3fadf15343778aa3dab5",
+    "url": "/tech-building/static/js/2.46c82f7a.chunk.js"
+  },
+  {
+    "revision": "a63220f3df6b692661dffbe895880882",
+    "url": "/tech-building/static/js/2.46c82f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784378a911200fc2fb0c",
+    "url": "/tech-building/static/js/main.a7f8772f.chunk.js"
+  },
+  {
+    "revision": "891fed6bc2488bd9e958",
+    "url": "/tech-building/static/js/runtime-main.14727502.js"
+  }
+]);
